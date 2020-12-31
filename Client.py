179056ea0ff96a8bd18fe0cecf20e934b097e8ae
Tal <Tal.meridor@gmail.com>
2020-12-31 13:07:05 +0200

    clean the code

--- Client.py
+++ Client.py
@@ -4,11 +4,12 @@ import time
 import getch
 import msvcrt
 
+
 class client():
 
     def openSocketUDP(self):
         """
-        docstring
+        this function open UDP socket for the client and recv the offers form the server
         """
         while(True):
             serverPort = 13117
@@ -17,29 +18,34 @@ class client():
             print ("Client started, listening for offer reqests...")
             rec=True
             clientSocket.bind(("",serverPort))
-            while (rec==True):
+            while (rec==True): #search for offers - if offer is found stop searching
                 data, addr = clientSocket.recvfrom(1024)
+                print(addr)
                 unpacked = struct.unpack("Ibh",data)
-                if (hex(unpacked[0])=='0xfeedbeef' and unpacked[1]==2):
-                    rec=False
+                if (hex(unpacked[0])=='0xfeedbeef' and unpacked[1]==2): #check if the offer is in the format
+                    rec=False #if so stop looking for offers
                 print("Received offer from %s, attempting to connect..."%addr[0])
-                self.openSocketTCP(unpacked[2],addr[0])
+                self.openSocketTCP(unpacked[2],addr[0]) #open tcp connection 
 
     def openSocketTCP(self,port,IP):
         """
-        docstring
+        this function open TCP socket for the client and try to connect the server 
+        params: port- the port number of the connection
+                IP- the IP number of the connection
         """
         try:
             serverName = IP
             serverPort = port
             clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            clientSocket.connect((serverName,serverPort))
+            clientSocket.connect((serverName,serverPort)) #connection to the TCP
             sentence = b"ATeam\n"
-            clientSocket.send(sentence)
+            clientSocket.send(sentence) #send to the sever my team name
             modifiedSentence = clientSocket.recv(1024)
-            print (modifiedSentence.decode())
+            print (modifiedSentence.decode()) 
             timeout = time.time()+10
-            while time.time()<=timeout:
+            while msvcrt.kbhit():#make sure that only the keys pressed in this 10 seconds are counted
+                msvcrt.getch()
+            while time.time()<=timeout: #the game is for 10 seconds
                 if(msvcrt.kbhit()):
                     clientSocket.send(getch.getch())
             modifiedSentence = clientSocket.recv(1024)
--- Server.py
+++ Server.py
@@ -7,63 +7,74 @@ import random
 class server():
     Group1 = []
     Group2 = []
+    threads = []
     sumGroup1=0
     sumGroup2=0
-    timeout=0
-    threads=[]
+
     
     def openSocketUDP(self):
         """
-        docstring
+        this function open UDP socket for the server and sent broadcast messages
+        send the massage every second for 10 seconds 
         """
         serverPort = 13117
-        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        clientSocket.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1) 
-        clientSocket.settimeout(10)
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        serverSocket.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1) 
+        serverSocket.settimeout(10) #need to send the massages for 10 seconds
         try:
             IP=socket.gethostbyname(socket.gethostname())
             print("Server started, listening on IP address %s"%IP)   
             while (True):
-                message = struct.pack('Ibh',0xfeedbeef,0x2,0x67)
-                clientSocket.sendto(message,('<broadcast>',serverPort))
+                message = struct.pack('Ibh',0xfeedbeef,0x2,0x67) #packet format
+                serverSocket.sendto(message,("'<broadcast>'",serverPort))
                 time.sleep(1)
         except socket.timeout():
-            clientSocket.close()
+            serverSocket.close()
         
 
     def openSocketTCP(self):
+        """
+        this function open TCP socket for the server and open threads for each user in the game
+        after 10 seconds start the games for the players
+        """
         serverPort = 103
         serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         serverSocket.bind(("",serverPort))
         while (True):
             UDPThread=threading.Thread(target = server1.openSocketUDP, args = ())
             UDPThread.start()
-            serverSocket.settimeout(10)
-            serverSocket.listen(1)
+            serverSocket.settimeout(10) #the connection part need to take 10 seconds
+            serverSocket.listen(1) 
             try:
                 while (True):
                     connectionSocket, addr = serverSocket.accept()
                     sentence = connectionSocket.recv(1024)
-                    choose=random.choice(["Group1","Group2"])
+                    choose=random.choice(["Group1","Group2"]) #choose Group randomly 
                     if (choose=="Group1"):
                         self.Group1.append(sentence)
                     else:
                         self.Group2.append(sentence)
-                    self.threads.append(threading.Thread(target=self.game,args=(connectionSocket,addr,choose)))
-            except socket.timeout:
+                    self.threads.append(threading.Thread(target=self.game,args=(connectionSocket,choose)))
+            except socket.timeout: #after 10 seconds open the games for the players
                 if (len(self.threads)>0):
                     for thread in self.threads:
                         thread.start()
                     for thread in self.threads:
                         thread.join()
-                    self.threads=[]
+                    #after the games is over set up the elements for the next game
+                    self.threads=[] 
                     print("Game over, sending out offer requests...")
                     self.Group1=[]
                     self.Group2=[]
                     self.sumGroup1=0
                     self.sumGroup2=0
                     
-    def game(self,connectionSocket,addr,choose):
+    def game(self,connectionSocket,choose):
+        """
+        this function is the game of the server count how much keys each group has prassed 
+        params: connectionSocket- the socket of each connection with user and the server
+                choose - the group of the player
+        """
         try:
             MSG = "Welcome to Keyboard Spamming Battle Royale."+"\n"
             MSG+="Group 1:"+"\n""=="+"\n"
@@ -74,15 +85,13 @@ class server():
                 MSG+=str(team.decode())
             MSG+="\nStart pressing keys on your keyboard as fast as you can!!"
             connectionSocket.send(str.encode(MSG))
-            sum=0
-            connectionSocket.settimeout(10)
+            sum=0 
+            connectionSocket.settimeout(10) #the game is only 10 seconds
             try:
                 while (True):
-                    sentence = connectionSocket.recv(1024)
-                    print(sentence)
-                    sum+=1
-                    print(sum)
-            except socket.timeout:
+                    connectionSocket.recv(1024)
+                    sum+=1 #add 1 to the sum when key is prassed 
+            except socket.timeout: #print the scores 
                 if (choose=="Group1"):
                     self.sumGroup1+=sum
                 else:
@@ -102,9 +111,9 @@ class server():
                 else:
                     for team in self.Group2:
                         MSG+=str(team.decode())
-                connectionSocket.send(str.encode(MSG))
+                connectionSocket.send(str.encode(MSG)) #send the scores to the players
                 connectionSocket.close()
-        except socket.error:
+        except socket.error: 
             print("Something wrong happen, try to re-connect")
             connectionSocket.close()
 
@@ -112,7 +121,6 @@ class server():
 if __name__ == "__main__":
 
     server1 = server()
-    server1.timeout = time.time()+10
     threading.Thread(target = server1.openSocketTCP, args = ()).start()
     
 
